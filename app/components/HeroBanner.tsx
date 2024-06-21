8fc12897ded8ed0c4294890f5793146310816120
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-06-22 00:49:36 +0500

    Setting up environment map

--- app/components/HeroBanner.tsx
+++ app/components/HeroBanner.tsx
@@ -7,6 +7,10 @@ import { Product } from "@/types/product";
 import { Banner } from "@/types/banner";
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+import { url } from "inspector";
+
 
 const HeroBanner: React.FC<Banner> = ({ ...bannerProps }) => {
 
@@ -20,28 +24,47 @@ const HeroBanner: React.FC<Banner> = ({ ...bannerProps }) => {
   useEffect(() => {
     const bannerSize = { width: canvasRef.current.clientWidth, height: canvasRef.current.clientHeight }
     const scene = new THREE.Scene();
-    // scene.background = new THREE.Color(0xcccccc);
     const camera = new THREE.PerspectiveCamera(75, bannerSize.width / bannerSize.height, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current, antialias: true, alpha: false });
+    const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current, antialias: true, alpha: true });
     renderer.setSize(bannerSize.width, bannerSize.height);
     console.log(bannerSize)
 
     camera.position.z = 50;
 
-    const ambientLight = new THREE.AmbientLight(0xcccccc, 0.5);
-    scene.add(ambientLight);
-
     const modelLoader = new GLTFLoader();
     modelLoader.load('/headphones.glb', (gltf) => {
       const loadedModel = gltf.scene;
-      // Adjust model position and scale as needed
-      loadedModel.position.y = -10;
+      loadedModel.position.set(0, -5, 0);
       loadedModel.scale.set(0.05, 0.05, 0.05);
       scene.add(loadedModel);
-      setModel(loadedModel); // Update model state
+      setModel(loadedModel);
+    });
+    
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.enablePan = false;
+    controls.enableZoom = false;
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = 2;
+
+    renderer.outputColorSpace = THREE.SRGBColorSpace;
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.toneMappingExposure = 2;
+
+    const skyboxTextureUrl = '/room.hdr';
+    const hdriLoader = new RGBELoader().setDataType(THREE.HalfFloatType)
+    hdriLoader.load(skyboxTextureUrl, function (texture) {
+      
+      texture.mapping = THREE.EquirectangularReflectionMapping;
+      texture.minFilter = THREE.LinearFilter;
+      texture.magFilter = THREE.LinearFilter;
+
+      scene.environment = texture;
+    
     });
 
     const animate = () => {
+      controls.update();
       renderer.render(scene, camera);
       requestAnimationFrame(animate);
     };
@@ -54,16 +77,30 @@ const HeroBanner: React.FC<Banner> = ({ ...bannerProps }) => {
 
   return (
     <>
-    {/* <div className="hero-banner-container">
-    </div> */}
-    <canvas className="hero-banner-canvas" ref={canvasRef} id="three-canvas"></canvas>
+      <canvas className="hero-banner-canvas" ref={canvasRef} id="three-canvas"></canvas>
+      <div className="hero-banner-container">
+        <div>
+          <p className="beats-solo">{smallText}</p>
+          <h3>{midText}</h3>
+          <h1>{largeText1}</h1>
+
+          <div>
+            <Link href={`/product/${product.toLowerCase()}`}>
+              <button type="button" onClick={() => { }}>{buttonText}</button>
+            </Link>
+            <div className="desc">
+              <h5>Description</h5>
+              <p>{desc}</p>
+            </div>
+          </div>
+        </div>
+      </div>
 
 
 
 
-    
+
     </>
-    
 
 
 
@@ -75,31 +112,6 @@ const HeroBanner: React.FC<Banner> = ({ ...bannerProps }) => {
 
 
 
-
-
-
-    // <div className="hero-banner-container">
-    //   <div>
-    //     <p className="beats-solo">{smallText}</p>
-    //     <h3>{midText}</h3>
-    //     <h1>{largeText1}</h1>
-    //     <img
-    //       src={urlFor(image).toString()}
-    //       alt="headphones"
-    //       className="hero-banner-image"
-    //     />
-
-    //     <div>
-    //       <Link href={`/product/${product.toLowerCase()}`}>
-    //         <button type="button" onClick={()=>{}}>{buttonText}</button>
-    //       </Link>
-    //       <div className="desc">
-    //         <h5>Description</h5>
-    //         <p>{desc}</p>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
